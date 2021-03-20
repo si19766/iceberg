428fbe3cd4b2f434818ca674217595bdfb0942ab
HarryHD123 <pn19345@bristol.ac.uk>
2021-03-20 15:23:39 +0000

    Added SpawnIceberg Func and Iceberg Class

--- Map/icebergs.py
+++ Map/icebergs.py
@@ -1,9 +1,18 @@
 import pygame
+from data_gen.py import IcebergGen()
 
-class Icebergs(object):
-    def __init__(self, x, y):
+# Randomly spawns Icebergs
+def SpawnIcebergs(NumIcebergs):
+    IcebergList = []
+    for i in range(NumIcebergs):
+        BergDimensions, BergCoords = IcebergGen()
+        Berg = Iceberg(BergDimensions(0), BergDimensions(1), BergCoords(0), BergCoords(1))
+        IcebergList.append(Berg)
+    return IcebergList
+
+class Iceberg(object):
+    def __init__(self, x_dimen, y_dimen, x_coord, y_coord):
         self.iceberg_colour = (255, 255, 255)
-        self.x, self.y = x, y
+        self.x_dimen, self.y_dimen = x_dimen, y_dimen
+        self.x_coord, self.y_coord = x_coord, y_coord
 
-    def create_iceberg_list(self):
-        pass
